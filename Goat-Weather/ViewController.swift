a2ed6c25d3667e1054b592a833cc511b8082eb60
Yefga <yefga.tp@icloud.com>
2023-12-12 17:49:14 +0700

    feat: Initial Commit, modify Swift Version & Minimum iOS Requirements

---
+++ Goat-Weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Goat-Weather
+//
+//  Created by pro on 12/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
